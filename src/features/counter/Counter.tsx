a4e866801642f6409aba31d6bf7905f48156d90e
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2023-03-13 09:07:22 +0100

    add initial structure

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -7,12 +7,10 @@ import {
   incrementByAmount,
   incrementAsync,
   incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+  } from './counterSlice';
 
 export function Counter() {
-  const count = useAppSelector(selectCount);
+  const count = useAppSelector((state)=>state.counter.value);
   const dispatch = useAppDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
@@ -20,44 +18,41 @@ export function Counter() {
 
   return (
     <div>
-      <div className={styles.row}>
+      <div>
         <button
-          className={styles.button}
+        
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
-        <span className={styles.value}>{count}</span>
+        <span>{count}</span>
         <button
-          className={styles.button}
+          
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           +
         </button>
       </div>
-      <div className={styles.row}>
+      <div>
         <input
-          className={styles.textbox}
+          
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
-          className={styles.button}
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
-          className={styles.asyncButton}
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
-          className={styles.button}
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
           Add If Odd
